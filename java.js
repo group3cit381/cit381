60aac57b8d0c60ae4a2be882a284340150bde99d
Group3 <43297718+group3cit381@users.noreply.github.com>
2020-12-14 18:53:57 -0500

    Update java.js
    Lukes and Vanessas comments

--- java.js
+++ java.js
@@ -1,6 +1,7 @@
+//List of defined variables
 let userScore= 0;
 let pcScore= 0;
-const userScore_span= document.getElementById('user_score');
+const userScore_span= document.getElementById('user_score'); //Receives and stores the element
 const pcScore_span= document.getElementById('pc_score');
 const scoreBoard_div= document.querySelector('score');
 const result_div= document.querySelector('.result p');
@@ -8,12 +9,16 @@ const rock_div= document.getElementById('rock');
 const paper_div= document.getElementById('paper');
 const scissors_div= document.getElementById('scissors');
 
+
+//Function that contains an array with the options and returns a random option between 3 objects
 function playersChoices(){
   const options= ['rock','paper','scissors'];
   const random= Math.floor(Math.random() * 3);
   const choices= options[random];
-  return (choices);
+  return (choices); //It returns the variable
 }
+
+//This function returns an image acoording to the element selected.
 function convertWord(option){
   if (option == 'rock'){
     return "Rock ✊";
@@ -24,6 +29,7 @@ function convertWord(option){
   }
 }
 
+//Function that announces if the user or the PC wins the move and increases its declared counter in the first lines
 function toWin(optionUser, optionPc){
   userScore++;
   userScore_span.innerHTML= userScore;
@@ -40,6 +46,7 @@ function toWin(optionUser, optionPc){
   }, 2000);
 }
 
+//Function that announces if the user or the PC lose the move and increases its declared counter in the first lines
 function toLose(optionUser, optionPc){
   pcScore++;
   pcScore_span.innerHTML= pcScore;
@@ -56,15 +63,18 @@ function toLose(optionUser, optionPc){
   }, 2000);
 }
 
+//Function that announces if the user or the PC choose the same option
 function tie(optionUser){
   result_div.innerHTML= "You both chose "+convertWord(optionUser)+"<br> It's a tie!!!";
   const option_div= document.getElementById(optionUser);
   option_div.classList.add('gray');
   setTimeout(function(){
     option_div.classList.remove("gray");
-  }, 2000);
+  }, 2000); 
 }
 
+
+//Function that receives the chosen options and compares it with the established cases to define which choice wins, loses or is a tie
 function game(option){
   const pcChoice= playersChoices();
   const userChoice= option;
@@ -87,6 +97,7 @@ function game(option){
   }
 }
 
+//Function that is executed when selecting an element with click.
 function main(){
   rock_div.addEventListener('click', () => game("rock"));
   paper_div.addEventListener('click', () => game("paper"));
